38ea888adbdaee1715df18ca152b7b562a05da83
Miguel Branco <mig.jcb@gmail.com>
2019-05-09 15:52:59 +0100

    Use document.title as well

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 let pollInterval = null;
 let previousUnread = [];
+let previousTitle = '';
 
 function init() {
     console.log('[SLACK-ATTENTION] Slack Attention init');
@@ -18,6 +19,7 @@ if (document.readyState !== 'loading') {
 console.log('[SLACK-ATTENTION] Slack Attention load');
 
 function checkUnread() {
+    let shouldNotify = false;
     let unreadChannels = document.querySelectorAll('.p-channel_sidebar__channel--unread, .p-message_pane__unread_banner');
     // Also seem to be included in the above:
     // let unreadPMs = document.querySelectorAll('.p-channel_sidebar__badge');
@@ -26,14 +28,27 @@ function checkUnread() {
     for (let i = 0; i < unreadChannels.length; ++i) {
 	console.log('[SLACK-ATTENTION] Considering unread', unreadChannels[i]);
 	if (!previousUnread.includes(unreadChannels[i])) {
-	    console.log('[SLACK-ATTENTION] Sending message to bg');
-	    chrome.runtime.sendMessage('drawAttention');
+	    shouldNotify = true;
 	    break;
 	}
     }
 
+    if (!shouldNotify) {
+	if ((previousTitle.indexOf('!') === -1 && document.title.indexOf('!') !== -1) ||
+	    (previousTitle.indexOf('*') === -1 && document.title.indexOf('*') !== -1)) {
+	    console.log('[SLACK-ATTENTION Title signals unread', previousTitle, document.title);
+	    shouldNotify = true;
+	}
+    }
+
+    if (shouldNotify) {
+	console.log('[SLACK-ATTENTION] Sending message to bg');
+	chrome.runtime.sendMessage('drawAttention');
+    }
+
     previousUnread.length = 0;
     previousUnread.push(...unreadChannels);
+    previousTitle = document.title;
 }
 
 
